d56764800cfef7171b08fa3d4c4e68a8fce673ab
Wandile Ndlovu <wandilen158@gmail.com>
2025-03-15 16:19:50 +0200

    Introduced professional level sass for rejecting jokes

--- index.py
+++ index.py
@@ -8,6 +8,13 @@ jokes = ["Why don't skeletons fight each other?They don't have the guts!",
          "I wasn't originally going to get a brain transplant,but then I changed my mind.",
 ]
 
+jokes2 = ["Ok, you must be the joke then",
+          "No joke? Okay, you must be the life of the party... or not.",
+          "No joke? What are you, too cool to crack a smile? Get over yourself.",
+          "Alright, no joke. I'm not sure why I expected you to have a sense of humor anyway.",
+          "No joke? You've got a lot of nerve rejecting comedy when it's clearly your only chance to seem interesting.",
+]
+
 def request():
     while True:
         userInput = input(" Want a joke?(Yes or no): ")
